f1a6ca5f43069ea2021e554a4970117a8bd51f7d
Adiissep <adisepriyadi0@gmail.com>
2023-07-31 08:56:54 +0700

    new project image processing

---
+++ transparentcolorRGB.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Status
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Panel1.BackColor = Color.FromArgb(150, 196, 15, 1)
+        Panel2.BackColor = Color.FromArgb(150, 248, 148, 6)
+        Panel3.BackColor = Color.FromArgb(150, 46, 204, 113)
+        Panel4.BackColor = Color.FromArgb(150, 30, 139, 195)
+
+    End Sub
+End Class
